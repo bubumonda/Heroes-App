5b06b03e6c4658bec8bd36318a0a1f465118a46d
bubumonda <giansebas.2015@gmail.com>
2023-09-24 16:30:42 -0500

    First commit

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Pipe } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from 'src/environments/environments';
+import { Observable, catchError, of, pipe } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes():Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+  getHeroById(id: string):Observable<Hero| undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error=> of(undefined))
+      );
+  }
+
+  getSuggestions(query:string):Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+
+  }
+}
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments ={
+  baseUrl: 'http://gian-mier.com/api'
+}
